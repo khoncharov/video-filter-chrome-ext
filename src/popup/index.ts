5f5a69acf3fa7902b71aec7d3a88288f22cd4356
Konstantin Honcharov <k.honcharov@gmail.com>
2023-04-06 14:33:58 +0300

    feat: add rxjs in config

--- src/popup/index.ts
+++ src/popup/index.ts
@@ -1,4 +1,5 @@
 /* eslint-disable implicit-arrow-linebreak */
+import { fromEvent, throttleTime } from 'rxjs';
 
 const detectBtn = document.querySelector('#detect-btn') as HTMLButtonElement;
 
@@ -6,13 +7,13 @@ function showContainer() {
   const videoContainer = document.querySelector<HTMLVideoElement>('video');
 
   if (videoContainer) {
-    videoContainer.style.filter = 'invert(1)';
+    videoContainer.style.border = '5px dashed magenta'; // CONST
 
     setTimeout(() => {
       if (videoContainer) {
-        videoContainer.style.filter = '';
+        videoContainer.style.border = 'none';
       }
-    }, 1000);
+    }, 500);
   }
 }
 
@@ -30,12 +31,12 @@ async function detectHandler() {
   }
 }
 
-detectBtn.addEventListener('click', detectHandler);
+fromEvent(detectBtn, 'click').pipe(throttleTime(1000)).subscribe(detectHandler);
 
 const defaultBtn = document.querySelector('#default-btn') as HTMLButtonElement;
-const brightBtn = document.querySelector('#bright-btn') as HTMLButtonElement;
+const applyBtn = document.querySelector('#bright-btn') as HTMLButtonElement;
 
-function changeVideo(brightness: number, contrast: number, saturate: number): void {
+function changeFilter(brightness: number, contrast: number, saturate: number): void {
   const filterStr = `brightness(${brightness}%) contrast(${contrast}%) saturate(${saturate}%)`;
   const videoContainer = document.querySelector<HTMLVideoElement>('video');
 
@@ -57,16 +58,20 @@ async function brightnessHandler(
 
     chrome.scripting.executeScript({
       target: { tabId: id },
-      func: changeVideo,
+      func: changeFilter,
       args: [brightness, contrast, saturate],
     });
   }
 }
 
-brightBtn.addEventListener('click', () => {
-  brightnessHandler(240, 108, 85);
-});
+fromEvent(applyBtn, 'click')
+  .pipe(throttleTime(50))
+  .subscribe(() => {
+    brightnessHandler(240, 108, 85);
+  });
 
-defaultBtn.addEventListener('click', () => {
-  brightnessHandler(100, 100, 100);
-});
+fromEvent(defaultBtn, 'click')
+  .pipe(throttleTime(50))
+  .subscribe(() => {
+    brightnessHandler(100, 100, 100);
+  });

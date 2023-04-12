9adc9f1234eadfa9a36a1ce102b3103a05bee6b1
Konstantin Honcharov <k.honcharov@gmail.com>
2023-04-12 16:49:25 +0300

    feat: add test

--- src/popup/app/services/data-events.ts
+++ src/popup/app/services/data-events.ts
@@ -2,6 +2,6 @@ import { DataEvent } from './types';
 
 export default class DataEventTarget extends EventTarget {
   notify(event: DataEvent) {
-    this.dispatchEvent(new CustomEvent(event));
+    this.dispatchEvent(new Event(event));
   }
 }
